76d1d6ae7347f526081fc2710a3d0bc73a1f4fb5
Alexandre <alexandresaynov@gmail.com>
2025-11-15 16:42:56 +0000

    Reapply "Fast-forward of menu branch"
    This reverts commit 2528ad3817cbae0bd618e520c8e442027ccb0096.

---
+++ menu.py
@@ -0,0 +1,30 @@
+def admin_menu():
+    while True:
+        print("\n=== MENU ROOT ADMIN ===")
+        print("1. Ver utilizadores")
+        print("2. Criar novo utilizador")
+        print("3. Gerir cargos e permissões")
+        print("4. Terminar sessão")
+        opcao = input("> ")
+
+
+
+def user_menu():
+    while True:
+        print("\n=== MENU UTILIZADOR ===")
+        print("1. Ver perfil")
+        print("2. Terminar sessão")
+        opcao = input("> ")
+
+
+
+
+def main_menu():
+    while True:
+        print("\n=== LOGIN / REGISTO ===")
+        print("1. Login")
+        print("2. Registar (apenas se for o primeiro)")
+        print("3. Sair")
+        opcao = input("> ")
+
+
