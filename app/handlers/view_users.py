34dfe0a219ba5821fa4c1a96870acc7db2730ce1
a89831 <a89831@ualg.pt>
2025-11-29 18:28:24 +0000

    Modified view_users.py to have an admin check before allowing editing

--- app/handlers/view_users.py
+++ app/handlers/view_users.py
@@ -1,18 +1,19 @@
 from app.handlers.edit_users import handle_edit_user
-def handle_view_all_users(db, auth):
+def handle_view_all_users(db, auth, permissions, logged_user):
     print("\n=== Users ===")
     users = db.get_all_users()
     for u in users:
         print(f"[{u.id}] {u.username} | {u.email} | {u.access_level}")
 
-    choice = input("\nEnter the ID of the user you want to edit, or press ENTER to go back: ").strip()
-    if not choice:
-        return
+    if permissions.has_permission(logged_user, "user.edit_all"):
+        choice = input("\nEnter the ID of the user you want to edit, or press ENTER to go back: ").strip()
+        if not choice:
+            return
 
-    try:
-        user_id = int(choice)
-    except ValueError:
-        print("Invalid ID.")
-        return
+        try:
+            user_id = int(choice)
+        except ValueError:
+            print("Invalid ID.")
+            return
 
-    handle_edit_user(db, auth, user_id)
+        handle_edit_user(db, auth, user_id)
